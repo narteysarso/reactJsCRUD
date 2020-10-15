7f6ee4f020c6a4eede84f4d6f0312858d77c0be9
Sars <narteysarso@gmail.com>
2020-10-15 13:00:59 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UsersCard from './UsersCard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    firstname: "Kofi",
+    lastname: "Ampah",
+    gender: "Male",
+    USERDATA: [ 
+      {
+        firstname: "Dummy firstname",
+        lastname: "Dummy lastname",
+        gender: "male"
+      },
+      {
+        firstname: "Dummy firstname 1",
+        lastname: "Dummy lastname 1 ",
+        gender: "male"
+      },
+      {
+        firstname: "Dummy firstname 2",
+        lastname: "Dummy lastname 2",
+        gender: "male"
+      },
+      {
+        firstname: "Dummy firstname 3",
+        lastname: "Dummy lastname 3",
+        gender: "male"
+      },
+    ]
+  };
+
+  removeUserData = (firstnameOfUserToDelete = "") => {
+    let numberItems = this.state.USERDATA.length; //4
+    let newUserData = [];
+    for(let position = 0; position < numberItems; position += 1){
+      // DO something
+      const user = this.state.USERDATA[position];
+      if( user.firstname ===  firstnameOfUserToDelete ) {
+        //Don't add to new list | Do nothing
+      }else{
+        //Add this data to new list
+        newUserData.push(user);
+      }
+
+    }
+
+    this.setState({USERDATA: newUserData});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //alert(`${this.state.firstname}, ${this.state.lastname}, ${this.state.gender}`)
+    this.setState({
+      USERDATA: [
+        ...this.state.USERDATA,
+        {
+          firstname: this.state.firstname,
+          lastname: this.state.lastname,
+          gender: this.state.gender
+        }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row" style={{ position: "absolute", justifyContent: "center", top: "50px" }}>
+          {/*<UsersCard firstname="Sedina" lastname="Fiagbedzi" gender="Female"/>
+          <UsersCard firstname="Loveck" lastname="Fanou" gender="Male"/>
+          <UsersCard firstname="Sarso" lastname="Nartey" gender="Male"/>*/}
+          {this.state.USERDATA.map((user) => {
+            return <UsersCard onDelete={(userFirstname) => this.removeUserData(userFirstname)} firstname={user.firstname} lastname={user.lastname} gender={user.gender} />
+          })}
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>First Name:</label>
+            <input name="firstname" placeholder="Enter First Name" value={this.state.firstname} onChange={(event) => {
+              this.setState({ firstname: event.target.value })
+            }} />
+            <label>Last Name:</label>
+            <input name="lastname" placeholder="Enter Last Name" value={this.state.lastname} onChange={(event) => {
+              this.setState({ lastname: event.target.value })
+            }} />
+            <label>Gender:</label>
+            <input name="gender" placeholder="Enter Your Gender" value={this.state.gender} onChange={(event) => {
+              this.setState({ gender: event.target.value })
+            }} />
+            <button htmlType="submit">Add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/UsersCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function UsersCard(props){
+    return(
+        <div className="card col-3">
+          <img src="https://codetraingh.com/static/logo-ec0b25dfea7daafa6e6245f9de4555a7.png" alt="" className="card-img-top"/>
+          <div class="card-body">
+            <h4>First name: {props.firstname} </h4>
+            <h4>Last name: {props.lastname} </h4>
+            <h4>Gender: {props.gender} </h4>
+            <p>Fellow at Codetrain</p>
+            <button className="btn btn-danger" onClick={() => props.onDelete(props.firstname)}>Delete</button>
+          </div>
+        </div>
+    );
+}
+
+export default UsersCard;
